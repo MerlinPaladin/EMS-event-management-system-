db93dc8e63a30261855a304301aaad929a7c70b5
MerlinPaladin <byblik67468@gmail.com>
2024-12-02 16:07:07 +0300

    Add files via upload

---
+++ sum/php_blocks/content.php
@@ -0,0 +1,31 @@
+<!-- основной контент -->
+<content>
+    <div class="js-sup-modal-wrapper">
+        <form action="">
+            <div class="l_x">
+                <label style="color: #fff; font-size: x-large;">Обратная связь</label>
+                <button class="exit_1">X</button>
+            </div>
+            <hr style="color: #fff; width: 100%; height: 3px;">
+            <br>
+            <input class="e-mail-h-form" type="text" value="" placeholder="E-mail" required>
+            <input class="comment-h-form" type="text" value="" placeholder="Комментарий" required>
+            <button class="submit_form_1">Отправить форму</button>
+        </form>
+    </div>
+    <!-- модальная форма с таблицей и мероприятиями -->
+    <div class="main_table_with_events">
+        <button class="exit_3">Закрыть</button>
+        <h1 class="view_events">Просмотр мероприятий</h1>
+        <h2 class="ch_date">Выберите дату</h2>
+        <form>
+            <input class="date_chooser" type="date">
+        </form>
+    </div>
+    <!-- блок с кнопками для обратной связи и ознакомления -->
+    <div class="js-modal-wrapper"></div>
+    <div class="info_and_help_actions">
+        <button class="support">Связь с нами</button>
+        <button class="about_us">Ознакомиться</button>
+    </div>
+</content>
