0f5608a7dc008699be73e669a4bf7c009e64002a
MerlinPaladin <byblik67468@gmail.com>
2024-12-02 16:06:36 +0300

    Delete sum directory

--- sum/php_blocks/footer.php
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-    if ($(document).height() <= $(window).height()) {
-        $(".page-footer").addClass("fixed-bottom");
-    }
-</script>
-<footer class="page-footer">worldofevents.ru|Все права защищены 2024</footer>
