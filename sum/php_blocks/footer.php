bb8226244ba11008849cd8747a78b13d9d951fca
MerlinPaladin <byblik67468@gmail.com>
2024-12-05 12:24:59 +0300

    Add files via upload

---
+++ sum/php_blocks/footer.php
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+    if ($(document).height() <= $(window).height()) {
+        $(".page-footer").addClass("fixed-bottom");
+    }
+</script>
+<footer class="page-footer">worldofevents.ru|Все права защищены 2024</footer>
