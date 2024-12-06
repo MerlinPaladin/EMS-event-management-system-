1306cffe6973abd87689c6e5d5ab4221bb4080c2
MerlinPaladin <byblik67468@gmail.com>
2024-12-06 14:41:13 +0300

    Add files via upload

---
+++ sum/php_blocks/header.php
@@ -0,0 +1,19 @@
+<!-- шапка -->
+<header>
+    <!-- логотип с названием -->
+    <div class="logo_and_name">
+        <a href="index.php"><img class="main_image" src="images/logo.png" width="100" height="100" alt="error"></a>
+        <div class="main_name">
+            <h1 class="site_name">МирСобытий</h1>
+        </div>
+    </div>
+    <!-- блок с кнопками(меню) -->
+    <div class="main_buttons">
+        <button type="button" id="activity">Активность</button>
+        <button type="button" id="events">Мероприятия</button>
+        <button type="button" id="all_active_events">Все активные мероприятия</button>
+        <a href="index.php"><button type="button" id="main_button">Главная</button></a>
+        <button type="button" id="register_button">Регистрация</button>
+    </div>
+    
+</header>
