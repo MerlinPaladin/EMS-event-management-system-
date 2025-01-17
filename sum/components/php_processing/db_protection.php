bff8853fc9d3b35980d2279932d50f32939f1711
MerlinPaladin <byblik67468@gmail.com>
2025-01-17 17:29:21 +0300

    Delete sum directory

--- sum/components/php_processing/db_protection.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$conn = new mysqli("MySQL-8.0", "root", "", "EMS");
-
-if ($conn->connect_error){
-    die("Ошибка: невозможно подключиться: " . $conn->connect_error);
-} else {
-    echo "<script>console.log('Connection established')</script>";
-}
