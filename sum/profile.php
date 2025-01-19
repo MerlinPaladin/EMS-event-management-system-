0f62a1c0caa485f528c7af65690bb6b6f4361f6a
MerlinPaladin <byblik67468@gmail.com>
2025-01-19 21:01:57 +0300

    Add files via upload

---
+++ sum/profile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/profile.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+<div id="container">
+        <?php require "./components/php_blocks/header.php"; ?>
+        <?php require "./components/php_blocks/footer.php" ?>
+    </div>
+    <script src="./assets/js/scripts.js"></script>
+</body>
+</html>
